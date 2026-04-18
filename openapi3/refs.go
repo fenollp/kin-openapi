340dd9cdd672bbc1155621b89d6cbaebb3116d1a
Pierre Fenoll <pierrefenoll@gmail.com>
2026-04-18 13:31:18 +0200

    openapi3: fix: schema sibling fields are an OpenAPI3.1 feature
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- openapi3/refs.go
+++ openapi3/refs.go
@@ -1072,7 +1072,7 @@ func (x *SchemaRef) Validate(ctx context.Context, opts ...ValidationOption) erro
 	}
 
 	if len(extras) != 0 {
-		if !validationOpts.jsonSchema2020ValidationEnabled {
+		if !validationOpts.isOpenAPI31OrLater {
 			return fmt.Errorf("extra sibling fields: %+v", extras)
 		}
 	}
